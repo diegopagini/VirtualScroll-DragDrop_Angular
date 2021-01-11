cde00800c58f4fd72efd6e2bb58ebddf01c58b62
Diego Pagini <diegopaginidev@gmail.com>
2021-01-11 17:06:11 -0300

    implementacion de drag & drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,24 @@ import { VirtualComponent } from './virtual/virtual.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragComponent } from './drag/drag.component';
+import { PaisesComponent } from './paises/paises.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, VirtualComponent],
+  declarations: [
+    AppComponent,
+    VirtualComponent,
+    DragComponent,
+    PaisesComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     DragDropModule,
     ScrollingModule,
+    HttpClientModule,
   ],
   exports: [DragDropModule, ScrollingModule],
   providers: [],
---
+++ src/app/paises/paises.component.ts
@@ -0,0 +1,26 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paises',
+  templateUrl: './paises.component.html',
+  styleUrls: ['./paises.component.css'],
+})
+export class PaisesComponent implements OnInit {
+  paises: any = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get(`https://restcountries.eu/rest/v2/lang/es`)
+      .subscribe((paises) => {
+        this.paises = paises;
+      });
+  }
+
+  drop(evento: CdkDragDrop<any>) {
+    moveItemInArray(this.paises, evento.previousIndex, evento.currentIndex);
+  }
+}
