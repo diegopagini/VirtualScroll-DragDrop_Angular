ac58fbe5bd0bd84104c7036e386efb5b6c3a2720
Diego Pagini <diegopaginidev@gmail.com>
2021-01-11 16:48:10 -0300

    virtual con animated style

--- src/app/virtual/virtual.component.ts
+++ src/app/virtual/virtual.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-virtual',
@@ -6,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./virtual.component.css'],
 })
 export class VirtualComponent implements OnInit {
+  @ViewChild(CdkVirtualScrollViewport) viewport: CdkVirtualScrollViewport;
   personas = Array(500).fill(0);
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  irAlFinal() {
+    this.viewport.scrollToIndex(this.personas.length);
+  }
+
+  irAlInicio() {
+    this.viewport.scrollToIndex(0);
+  }
+
+  irALaMitad() {
+    this.viewport.scrollToIndex(this.personas.length / 2);
+  }
 }
